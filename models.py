d5ee7a6b8604572ca940316d2d32b13ffca88f53
RaphaelSilv4 <146749859+RaphaelSilv4@users.noreply.github.com>
2024-04-25 17:46:54 -0300

    Uso do metodo get usando o serialize para assim poder fazer a listagem de usuarios.

--- models.py
+++ models.py
@@ -2,11 +2,48 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from config.database import db
 
+
 class Cliente(db.Model):
     __tablename__ = "usuario_cliente"
 
-    id = mapped_column(Integer, primary_key=True,autoincrement=True)
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
     nome = mapped_column(String(30), nullable=False)
-    def __init__(self, nome):
-        self.nome = nome
+    senha = mapped_column(String(30), nullable=False)
+    email = mapped_column(String(90), nullable=False)
+    cargo = mapped_column(String(90), default='usuario_cliente')
 
+    def __init__(self, nome, senha, email):
+        self.nome = nome
+        self.senha = senha
+        self.email = email
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'email': self.email,
+            'cargo': self.cargo
+        }
+
+
+class Cargo(db.Model):
+    __tablename__ = "cargo"
+
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    nome = mapped_column(String(30), nullable=False)
+
+    def __repr__(self):
+        return f"<Cargo {self.nome}>"
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'email': self.email,
+            'cargo': self.cargo
+        }
+
+    def __init__(self, nome, senha, email):
+        self.nome = nome
+        self.senha = senha
+        self.email = email
---
+++ routes/cliente_routes.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, jsonify
+from config.database import db
+from models import Cliente
+
+cliente_bp = Blueprint('cliente_bp', __name__, url_prefix='/routes')
+
+
+@cliente_bp.route("/cliente", methods=("GET", "POST"))
+def registrar_cliente(cliente=None):
+    # listagem dos clientes
+    if request.method == "GET":
+        clientes = Cliente.query.all()
+        return jsonify([cliente.serialize() for cliente in clientes]), 200
+    # adicionar novo cliente
+    elif request.method == "POST":
+        data = request.get_json()
+        cliente_nome = data["nome"]
+        cliente_email = data['email']
+        cliente_senha = data['senha']
+        cliente = Cliente(
+            nome=cliente_nome,
+            email=cliente_email,
+            senha=cliente_senha
+        )
+        if not cliente_nome or not cliente_email or not cliente_senha:
+            return jsonify('Faltam dados obrigatórios'), 200
+
+        db.session.add(cliente)
+        db.session.commit()
+        return jsonify("Cliente cadastrado com sucesso"), 200
